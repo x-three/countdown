ffe65cef4262027d00103f93bcfe1878567c44a8
Dizzy <vladimir.yaroslavl@gmail.com>
2017-09-03 20:15:44 +0300

    0.3.1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Countdown
+* /?date=\<year>:\<month>:\<day>:\<hours>:\<minutes>:\<seconds>[&forward]
+* /?time=\<days>:\<hours>:\<minutes>:\<seconds>[&forward]
+* /?forward
--- js/script.js
+++ js/script.js
@@ -2,10 +2,135 @@ jQuery(function($) {
     'use strict';
 
     var oParams = new window.webapp.Params(),
-        ny = new Date((new Date()).getFullYear() + 1, 0, 1),
-        time = Math.max(oParams.date || 0, oParams.time || 0) || Math.floor((ny.getTime() - Date.now()) / 1000),
-        oCountdown = new window.webapp.Countdown($('.b-countdown').eq(0), time);
+        nyDate = new Date((new Date()).getFullYear() + 1, 0, 1),
+        nyLeft = Math.floor((nyDate.getTime() - Date.now()) / 1000),
+        time = oParams.date || oParams.time || oParams.obj.forward === undefined && nyLeft || 0;
+
+    new window.webapp.Countdown($('.b-countdown').eq(0), time, oParams.obj.forward !== undefined);
 });
+
+
+
+(function($) {
+    'use strict';
+
+    (window.webapp || (window.webapp = {})).Countdown = Countdown;
+
+
+    function Countdown(el, time, forward) {
+        $.extend(this, {
+            $el:     $(el).addClass('b-countdown'),
+            time:    time || 0,
+            forward: forward || false,
+            offset:  Date.now() % 1000,
+            aNames:  ['hours', 'minutes', 'seconds'],
+            oItems:  {}
+        });
+
+        if (time > 24 * 3600) this.aNames.unshift('days');
+        this.initialize();
+        this.tick();
+    }
+
+
+    Countdown.prototype.initialize = function() {
+        var that = this,
+            oTime = this.getTimeObj(),
+            $item;
+
+        $.each(this.aNames, function(index, name) {
+            $item = $('<ul></ul>').addClass('m-' + name).appendTo(that.$el);
+            that.oItems[name] = new window.webapp.FlipNumber($item, oTime[name], 2);
+        });
+    };
+
+
+    Countdown.prototype.getTimeObj = function(time) {
+        var seconds = time !== undefined ? time : this.time;
+        return {
+            days: Math.floor(seconds / (24 * 3600)),
+            hours: Math.floor(seconds % (24 * 3600) / 3600),
+            minutes: Math.floor(seconds % 3600 / 60),
+            seconds: seconds % 60
+        };
+    };
+
+
+    Countdown.prototype.tick = function() {
+        var that = this,
+            duration = 1000 + (this.offset - Date.now() % 1000),
+            oTime = this.getTimeObj(this.time + (this.forward ? 1 : -1)),
+            aDeferred = [];
+
+        $.each(this.aNames, function(index, name) {
+            if (that.oItems[name].value !== oTime[name]) {
+                aDeferred.push(that.oItems[name].flip(oTime[name], duration));
+            }
+        });
+
+        $.when.apply(null, aDeferred).done(function() {
+            that.time += that.forward ? 1 : -1;
+            that.tick();
+        });
+    };
+}(jQuery));
+
+
+
+(function($) {
+    'use strict';
+
+    (window.webapp || (window.webapp = {})).FlipNumber = FlipNumber;
+
+
+    function FlipNumber(el, value, digits) {
+        $.extend(this, {
+            $el: $(el).addClass('b-flip-number'),
+            value: value,
+            digits: Math.max(digits || 0, value.toString().length),
+            aItems: [],
+            busy: false
+        });
+        this.initialize();
+    }
+
+
+    FlipNumber.prototype.initialize = function() {
+        var aValue = this.value.toString().split('').reverse(),
+            $item;
+
+        for (var i = 0; i < this.digits; i++) {
+            $item = $('<li>' + (aValue[i] || 0) + '</li>').prependTo(this.$el);
+            this.aItems.push(new window.webapp.Flipper($item));
+        }
+    };
+
+
+    FlipNumber.prototype.flip = function(value, duration) {
+        if (this.busy) return;
+        this.busy = true;
+
+        var that = this,
+            aOldValue = this.value.toString().split('').reverse(),
+            aNewValue = value.toString().split('').reverse(),
+            aDeferred = [],
+            ready = $.Deferred();
+
+        for (var i = 0; i < this.digits; i++) {
+            if (aOldValue[i] !== aNewValue[i]) {
+                aDeferred.push(this.aItems[i].flip(aNewValue[i] || 0, duration));
+            }
+        }
+
+        $.when.apply(null, aDeferred).done(function() {
+            that.value = value;
+            that.busy = false;
+            ready.resolve();
+        });
+
+        return ready;
+    };
+}(jQuery));
 
 
 
@@ -84,127 +209,6 @@ jQuery(function($) {
 (function($) {
     'use strict';
 
-    (window.webapp || (window.webapp = {})).FlipNumber = FlipNumber;
-
-
-    function FlipNumber(el, value, digits) {
-        $.extend(this, {
-            $el: $(el).addClass('b-flip-number'),
-            value: value,
-            digits: Math.max(digits || 0, value.toString().length),
-            aItems: [],
-            busy: false
-        });
-        this.initialize();
-    }
-
-
-    FlipNumber.prototype.initialize = function() {
-        var aValue = this.value.toString().split('').reverse(),
-            $item;
-
-        for (var i = 0; i < this.digits; i++) {
-            $item = $('<li>' + (aValue[i] || 0) + '</li>').prependTo(this.$el);
-            this.aItems.push(new window.webapp.Flipper($item));
-        }
-    };
-
-
-    FlipNumber.prototype.flip = function(value, duration) {
-        if (this.busy) return;
-        this.busy = true;
-
-        var that = this,
-            aOldValue = this.value.toString().split('').reverse(),
-            aNewValue = value.toString().split('').reverse(),
-            aDeferred = [],
-            ready = $.Deferred();
-
-        for (var i = 0; i < this.digits; i++) {
-            if (aOldValue[i] !== aNewValue[i]) {
-                aDeferred.push(this.aItems[i].flip(aNewValue[i] || 0, duration));
-            }
-        }
-
-        $.when.apply(null, aDeferred).done(function() {
-            that.value = value;
-            that.busy = false;
-            ready.resolve();
-        });
-
-        return ready;
-    };
-}(jQuery));
-
-
-
-(function($) {
-    'use strict';
-
-    (window.webapp || (window.webapp = {})).Countdown = Countdown;
-
-
-    function Countdown(el, time) {
-        $.extend(this, {
-            $el: $(el).addClass('b-countdown'),
-            time: Math.max(0, time),
-            offset: Date.now() % 1000,
-            aNames: ['days', 'hours', 'minutes', 'seconds'],
-            oItems: {}
-        });
-        this.initialize();
-        this.tick();
-    }
-
-
-    Countdown.prototype.initialize = function() {
-        var that = this,
-            oTime = this.getTimeObj(),
-            $item;
-
-        $.each(this.aNames, function(index, name) {
-            $item = $('<ul></ul>').addClass('m-' + name).appendTo(that.$el);
-            that.oItems[name] = new window.webapp.FlipNumber($item, oTime[name], 2);
-        });
-    };
-
-
-    Countdown.prototype.getTimeObj = function(time) {
-        var seconds = time !== undefined ? time : this.time;
-        return {
-            days: Math.floor(seconds / (24 * 3600)),
-            hours: Math.floor(seconds % (24 * 3600) / 3600),
-            minutes: Math.floor(seconds % 3600 / 60),
-            seconds: seconds % 60
-        };
-    };
-
-
-    Countdown.prototype.tick = function() {
-        if (this.time <= 0) return;
-
-        var that = this,
-            duration = 1000 + (this.offset - Date.now() % 1000),
-            oTime = this.getTimeObj(this.time - 1),
-            aDeferred = [];
-
-        $.each(this.aNames, function(index, name) {
-            if (that.oItems[name].value !== oTime[name]) {
-                aDeferred.push(that.oItems[name].flip(oTime[name], duration));
-            }
-        });
-
-        $.when.apply(null, aDeferred).done(function() {
-            that.time--;
-            that.tick();
-        });
-    };
-}(jQuery));
-
-
-(function($) {
-    'use strict';
-
     (window.webapp || (window.webapp = {})).Params = Params;
 
 
@@ -214,18 +218,16 @@ jQuery(function($) {
             if (this.obj.date) this.date = this.parseDate(this.obj.date);
             if (this.obj.time) this.time = this.parseTime(this.obj.time);
         }
-        console.log(this);
     }
 
 
     Params.prototype.initialize = function() {
         var that = this;
-        if (window.location.search === '') {
-            this.obj = null;
-        } else {
-            this.obj = {};
+        this.obj = {};
+        if (window.location.search !== '') {
             $.each(window.location.search.substr(1).split('&'), function(index, el) {
                 var separator = el.indexOf('=');
+                if (separator === -1) separator = el.length;
                 that.obj[el.substr(0, separator)] = el.substr(separator + 1);
             });
         }
